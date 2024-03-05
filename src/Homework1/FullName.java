8feabac1ea3a18b45c9fda8b20c240f8281f1d04
Bamiho <likrensse@gmail.com>
2024-03-05 22:42:20 +0200

    Exercise

--- src/Homework1/FullName.java
+++ src/Homework1/FullName.java
@@ -1,22 +1,12 @@
-public class Main {
+package Homework1;
+
+public class FullName {
     public static void main(String[] args) {
         String name1 = ("Bozhidara ");
         String name2 = ("Vasileva ");
         String name3 = ("Mihova");
         String fullName = (name1 + name2 + name3);
         System.out.println(fullName);
-
-//HW2
-
-        int a = 5;
-        int b = 5;
-        int c = 9;
-        int P = (a + b + c);
-        System.out.println("The perimeter equals " + P);
-
-//HW4
-
-
     }
 }
 
---
+++ src/Homework1/Perimeter.java
@@ -0,0 +1,15 @@
+package Homework1;
+
+public class Perimeter {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 5;
+        int c = 9;
+        int P = (a + b + c);
+        System.out.println("The perimeter equals " + P);
+
+
+
+    }
+}
